77f686377b253a299d346868dbb721ecfda02260
Altaf Multani <altafmultani16@gmail.com>
2022-09-20 14:19:30 +0530

    first commit

---
+++ SlidingImageSwiftUI/SlidingImageSwiftUI/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  SlidingImageSwiftUI
+//
+//  Created by Javid Multani on 29/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var pageIndex = 0
+    private let pages: [Page] = Page.samplePages
+    private let dotAppearance = UIPageControl.appearance()
+    
+    var body: some View {
+        TabView(selection: $pageIndex) {
+            ForEach(pages) { page in
+                VStack {
+                    Spacer()
+                    PageView(page: page)
+                    Spacer()
+                    if page == pages.last {
+                        Button("Sign up!", action: goToZero)
+                            .buttonStyle(.bordered)
+                    } else {
+                        Button("next", action: incrementPage)
+                            .buttonStyle(.borderedProminent)
+                    }
+                    Spacer()
+                }
+                .tag(page.tag)
+            }
+        }
+        .animation(.easeInOut, value: pageIndex)// 2
+        .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+        .tabViewStyle(PageTabViewStyle())
+        .onAppear {
+            dotAppearance.currentPageIndicatorTintColor = .black
+            dotAppearance.pageIndicatorTintColor = .gray
+        }
+    }
+    
+    func incrementPage() {
+        pageIndex += 1
+    }
+    
+    func goToZero() {
+        pageIndex = 0
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlidingImageSwiftUI/SlidingImageSwiftUI/SlidingImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingImageSwiftUIApp.swift
+//  SlidingImageSwiftUI
+//
+//  Created by Javid Multani on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SlidingImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
